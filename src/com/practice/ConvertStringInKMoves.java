ca1af61cb83e195bf3307f6347836864da5643d8
vshar6 <vikrantsharma1986@gmail.com>
2021-01-20 21:32:24 +0530

    Done  - can-convert-string-in-k-moves

--- src/com/practice/ConvertStringInKMoves.java
+++ src/com/practice/ConvertStringInKMoves.java
@@ -1,62 +1,44 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /* https://leetcode.com/problems/can-convert-string-in-k-moves/ */
 
 /* Work in Progress */
 
-
+/*
+*  String s is the first String
+* String t is the second String
+* k is the allowed number of passes
+* */
 public class ConvertStringInKMoves {
 
     public static boolean canConvertString(String s, String t, int k) {
-
-        if(s.equals(t)) {
-            return true;
-        }
-
-        char [] firstCharArray = s.toCharArray();
-        char [] secondCharArray = t.toCharArray();
-
-        int [] diffIndexes = new int [firstCharArray.length];
-        Set<Integer> setOfPasses = new HashSet<>();
-
-        setOfPasses.add(0);
-
-        //store dissimilar indexes
-        if(firstCharArray.length != secondCharArray.length || k<firstCharArray.length) {
+        if (s == null || t == null || s.length() != t.length() || k < 0) {
             return false;
         }
-
-        //aab bbb
-        for(int i = 0;i < firstCharArray.length;i++) {
-            diffIndexes[i] = secondCharArray[i]-firstCharArray[i];
-            System.out.println("for i="+i+", difference between "+secondCharArray[i]+" and "+firstCharArray[i]+" is "+diffIndexes[i]);
-
-            if(diffIndexes[i] > k) {
-                System.out.println(diffIndexes[i]+" pass is greater than the allowed number of passes:"+k);
-                return false;
+        Map<Integer, Integer> shiftsToFreq = new HashMap<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                int shift = t.charAt(i) - s.charAt(i);
+                if (shift < 0) {
+                    shift += 26;
+                }
+                shiftsToFreq.put(shift, shiftsToFreq.getOrDefault(shift, 0) + 1);
             }
-
-            if((diffIndexes[i]!=0) && setOfPasses.contains(diffIndexes[i])) {
-                if(k>=diffIndexes[i] + 26) {
-                    setOfPasses.add(26 + diffIndexes[i]);
-                } else {
-                    System.out.println(diffIndexes[i] + " pass has already been executed, it can not be executed again");
-                    return false;
-                }
-            } else if(diffIndexes[i] < 0){
-                if((26 + diffIndexes[i])> k) {
-                    System.out.println((26 + diffIndexes[i])+" is greater than total number of allowed passes:"+k);
-                    return false;
-                } else {
-                    setOfPasses.add(26 + diffIndexes[i]);
-                }
-
-            } else {
-                setOfPasses.add(diffIndexes[i]);
+        }
+        for (Map.Entry<Integer, Integer> entry : shiftsToFreq.entrySet()) {
+            int shift = entry.getKey();
+            int freq = entry.getValue();
+            if (shift + (freq-1)*26 > k) {
+                return false;
             }
         }
         return true;
+    }
 
+    /*a-t=-19;t-v=-2;m-b=11;x-j=14;z-h=18;j-v=-12;k-j=1;z-d=22*/
+    public static void main(String [] args) {
+        System.out.println(canConvertString("jicfxaa","ocxltbp",15));
+        System.out.println('m'-'b');
     }
 }
