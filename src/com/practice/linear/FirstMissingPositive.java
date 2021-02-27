a990a50a24a32d4a39624a6b0a13cb8ec3e03981
vshar6 <vikrantsharma1986@gmail.com>
2021-02-28 01:12:50 +0530

    done - /first-missing-positive/

---
+++ src/com/practice/linear/FirstMissingPositive.java
@@ -0,0 +1,104 @@
+package linear;
+
+import java.util.*;
+
+// https://leetcode.com/problems/first-missing-positive/
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int [] nums) {
+        Arrays.sort(nums);
+        boolean isallNegative = true;
+        Set<Integer> uniqueNumbers = new HashSet<>(nums.length);
+        int first_missing_positive = 1;
+        for(int index = 0;index < nums.length;index++) {
+            //System.out.println("[" + index + "]=" + nums[index]);
+            if (nums[index] > 0) {
+                isallNegative = false;
+                if (nums[index] <= first_missing_positive && !uniqueNumbers.contains(nums[index])) {
+                    first_missing_positive = first_missing_positive +1;
+                }
+                uniqueNumbers.add(nums[index]);
+            }
+        }
+        System.out.println("first_missing_positive:" + first_missing_positive);
+        return isallNegative?1:first_missing_positive;
+    }
+
+//    public int firstMissingPositive(int [] nums) {
+//
+//        boolean isallNegative = true;
+//        Set<Integer> uniqueNumbers = new TreeSet<>();
+//        int first_missing_positive = 1;
+//        for(int index = 0;index < nums.length;index++) {
+//            uniqueNumbers.add(nums[index]);
+//        }
+//
+//        for(Integer element: uniqueNumbers) {
+//            if (element > 0) {
+//                isallNegative = false;
+//                if(element <=first_missing_positive) {
+//                    first_missing_positive = first_missing_positive +1;
+//                }
+//            }
+//        }
+//
+//        System.out.println("first_missing_positive:" + first_missing_positive);
+//        return isallNegative?1:first_missing_positive;
+//    }
+
+
+    public static void main(String [] args) {
+        int [] array = {};
+        new FirstMissingPositive().firstMissingPositive(array);
+    }
+
+    /*
+    * public int firstMissingPositive(int [] nums) {
+        Arrays.sort(nums);
+        boolean isallNegative = true;
+        Set<Integer> uniqueNumbers = new HashSet<>(nums.length);
+        int first_missing_positive = 1;
+        for(int index = 0;index < nums.length;index++) {
+            //System.out.println("[" + index + "]=" + nums[index]);
+            if (nums[index] > 0) {
+                isallNegative = false;
+                if (nums[index] <= first_missing_positive && !uniqueNumbers.contains(nums[index])) {
+                    first_missing_positive = first_missing_positive +1;
+                }
+                uniqueNumbers.add(nums[index]);
+            }
+        }
+        System.out.println("first_missing_positive:" + first_missing_positive);
+        return isallNegative?1:first_missing_positive;
+    }
+    }
+    * */
+
+    /*
+    * public int firstMissingPositive(int [] nums) {
+         boolean isallNegative = true;
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        int first_missing_positive = 1;
+        for(int index = 0;index < nums.length;index++) {
+            uniqueNumbers.add(nums[index]);
+        }
+
+        for(Integer element: uniqueNumbers) {
+            if (element > 0) {
+                isallNegative = false;
+                if(element <=first_missing_positive) {
+                    first_missing_positive = first_missing_positive +1;
+                }
+            }
+        }
+
+        System.out.println("first_missing_positive:" + first_missing_positive);
+        return isallNegative?1:first_missing_positive;
+    }
+    * */
+
+
+
+
+}
