aa5452d6ee6b616cb6846566c99f068f96d2e1c9
vshar6 <vikrantsharma1986@gmail.com>
2024-11-18 11:32:45 +0530

    solved with adge case handling - longest-palindromic-substring

--- src/com/practice/linear/LongestPalindrome.java
+++ src/com/practice/linear/LongestPalindrome.java
@@ -19,8 +19,10 @@ Example 2:
 
 package linear;
 
-public class LongestPalindrome {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class LongestPalindrome {
 
     public static String longestPalindrome(String s) {
         String longestPalindrome = "";
@@ -29,13 +31,11 @@ public class LongestPalindrome {
         }
 
         char [] _letters = s.toCharArray();
-        for(int index=0;index<_letters.length/2;index++) {
+        for(int index=0;index<_letters.length;index++) {
             int reverseCounter = s.length()-1;
             while(reverseCounter != 0 && reverseCounter > index) {
-
                 if( _letters[reverseCounter] == _letters[index] ) {
                     String tempString = s.substring(index,reverseCounter+1);
-                    System.out.println(tempString);
                     if(isPalindrome(tempString) && tempString.length() > longestPalindrome.length() ) {
                         longestPalindrome = tempString;
                     }
@@ -44,35 +44,57 @@ public class LongestPalindrome {
                 reverseCounter--;
             }
         }
+        //  System.out.println("longestPalindrome:"+longestPalindrome);
 
+        if(longestPalindrome.equals("")) {
+            longestPalindrome = String.valueOf(s.charAt(0));
+        }
         return longestPalindrome;
     }
 
-    private static boolean isPalindrome(String word) {
-        int length = word.length();
-        char[] _reverse = new char[length];
-        int counter =0;
-        char [] _letters = word.toCharArray();
+//     private static boolean isPalindrome(String word) {
+//         int length = word.length();
+//         char[] _reverse = new char[length];
+//         int counter =0;
+//         char [] _letters = word.toCharArray();
 
-        while(length!=0) {
-            length--;
-            _reverse[length] = _letters[counter];
-            counter++;
+//         while(length!=0) {
+//             length--;
+//             _reverse[length] = _letters[counter];
+//             counter++;
+//         }
+
+//         String reverse = new String(_reverse);
+// //        if(reverse.equals(word)) {
+// //            System.out.println("Possible palindrome:"+reverse);
+// //        }
+
+    //         return reverse.equals(word);
+//     }
+    private static boolean isPalindrome(String str)
+    {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
         }
-
-        String reverse = new String(_reverse);
-        if(reverse.equals(word)) {
-            System.out.println("Possible palindrome:"+reverse);
-        }
-
-        return reverse.equals(word);
+        return true;
     }
 
 
-
     public static void main(String [] args) {
-        System.out.println("answer="+longestPalindrome("cbbd"));
+        long startTime = System.currentTimeMillis();
+        Runtime rt = Runtime.getRuntime();
+        long total_mem = rt.totalMemory();
+        String input = "jglknendplocymmvwtoxvebkekzfdhykknufqdkntnqvgfbahsljkobhbxkvyictzkqjqydczuxjkgecdyhixdttxfqmgksrkyvopwprsgoszftuhawflzjyuyrujrxluhzjvbflxgcovilthvuihzttzithnsqbdxtafxrfrblulsakrahulwthhbjcslceewxfxtavljpimaqqlcbrdgtgjryjytgxljxtravwdlnrrauxplempnbfeusgtqzjtzshwieutxdytlrrqvyemlyzolhbkzhyfyttevqnfvmpqjngcnazmaagwihxrhmcibyfkccyrqwnzlzqeuenhwlzhbxqxerfifzncimwqsfatudjihtumrtjtggzleovihifxufvwqeimbxvzlxwcsknksogsbwwdlwulnetdysvsfkonggeedtshxqkgbhoscjgpiel";
+        //System.out.println("answer="+longestPalindrome("kqwertytrewq"));
+        System.out.println("longestPalindrome:"+longestPalindrome(input));
+        System.out.println("elapsetime="+( System.currentTimeMillis()-startTime));
 
+        System.out.println("Amount of used memory: " + (total_mem - rt.freeMemory()));
        // System.out.println("answer="+isPalindrome("21a12321a12"));
     }
 }
