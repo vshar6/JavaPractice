d124495b564eacf40be97b91ff1b925d1055da18
vshar6 <vikrantsharma1986@gmail.com>
2024-11-20 14:38:16 +0530

    solved - https://leetcode.com/problems/container-with-most-water/

---
+++ src/com/practice/linear/ContainerWithMostWater.java
@@ -0,0 +1,57 @@
+/*
+
+https://leetcode.com/problems/container-with-most-water/
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+Example 1:
+    Input: height = [1,8,6,2,5,4,8,3,7]
+    Output: 49
+    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+    Input: height = [1,1]
+    Output: 1
+ */
+
+package linear;
+
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+
+        int maxArea = 0;
+
+        for(int index = 0;index < height.length; index++) {
+
+            int length = 0;
+            while(length < height.length) {
+                if(length!=index) {
+                    int effectiveHeight = height[index] > height[length]?height[length]:height[index];
+                    int effectiveLength = Math.abs(length-index);
+                    int tempArea = effectiveHeight * effectiveLength;
+                    //System.out.println("effectiveHeight for height["+index+"]:"+height[index]+" and height["+length+"]"+height[length]+", distance:"+effectiveLength+" is:"+effectiveHeight+". Area:"+tempArea);
+
+                    if(tempArea > maxArea) {
+                        maxArea = tempArea;
+                    }
+                }
+                length++;
+            }
+            System.out.println("max area for height["+index+"]:"+maxArea);
+        }
+        System.out.println("max area:"+maxArea);
+        return maxArea;
+    }
+
+    public static void main(String [] args) {
+        //int[] height = {1,8,6,2,5,4,8,3,7};
+        int[] height = {1,1};
+        maxArea(height);
+    }
+}
